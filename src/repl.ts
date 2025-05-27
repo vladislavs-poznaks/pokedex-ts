554f7a3451434aa971a8be1e2d4db9d426bcf2dd
vladislavs-poznaks <vladislavs.poznaks@viasmsgroup.com>
2025-05-27 14:45:30 +0300

    added repl basic functionality

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,25 @@
+import { createInterface } from "readline"
+
+export const startREPL = (): void => {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "pokedex > ",
+    })
+
+    rl.prompt()
+
+    rl.on("line", (input) => {
+        const words = cleanInput(input)
+
+        if (words.length) {
+            console.log(`Your command was: ${words[0]}`)
+        }
+
+        rl.prompt()
+    })
+}
+
 export const cleanInput = (input: string): string[] => {
     return input
         .trim()
